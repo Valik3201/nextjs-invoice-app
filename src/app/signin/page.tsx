7ae2dcc46f91a86ef4e1f403287a2e7171d5e717
Valik <valik3201@gmail.com>
2024-05-27 23:24:07 +0200

    feat(ui): Updated signup and signin pages to work with Redux

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,24 +1,21 @@
 "use client";
 import React, { useState } from "react";
-import signIn from "../../firebase/auth/signin";
+import { signIn } from "@/src/lib/features/auth/authOperations";
 import { useRouter } from "next/navigation";
+import { useAppDispatch } from "@/src/lib/hooks";
+import InputField from "@/src/components/InputField";
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
+  const dispatch = useAppDispatch();
 
   const handleForm = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const { result, error } = await signIn(email, password);
+    dispatch(signIn({ email, password }));
 
-    if (error) {
-      console.log(error);
-      return;
-    }
-
-    console.log(result);
     router.push("/invoices");
   };
 
@@ -28,28 +25,24 @@ export default function SignIn() {
         <div className="form-wrapper">
           <h1 className="mt-60 mb-30">Sign In</h1>
           <form onSubmit={handleForm} className="form">
-            <label htmlFor="email">
-              <p>Email</p>
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                type="email"
-                name="email"
-                id="email"
-                placeholder="example@mail.com"
-              />
-            </label>
-            <label htmlFor="password">
-              <p>Password</p>
-              <input
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                type="password"
-                name="password"
-                id="password"
-                placeholder="password"
-              />
-            </label>
+            <InputField
+              label="Email"
+              name="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="example@mail.com"
+            />
+
+            <InputField
+              label="Password"
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="example@mail.com"
+            />
+
             <button type="submit">Sign In</button>
           </form>
         </div>
