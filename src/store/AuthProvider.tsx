773d59fea3aad656db9218ecff47d104b8d85de9
Valik <valik3201@gmail.com>
2024-05-28 00:28:24 +0200

    refactor(auth): Refactored AuthProvider

--- src/store/AuthProvider.tsx
+++ src/store/AuthProvider.tsx
@@ -3,11 +3,11 @@ import { useEffect } from "react";
 import { listenToAuthChanges } from "../lib/features/auth/authOperations";
 import { useAppDispatch, useAppSelector } from "../lib/hooks";
 
-interface AuthProviderProps {
+export default function AuthProvider({
+  children,
+}: {
   children: React.ReactNode;
-}
-
-const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+}) {
   const dispatch = useAppDispatch();
   const { loading } = useAppSelector((state) => state.auth);
 
@@ -20,6 +20,4 @@ const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   }
 
   return <>{children}</>;
-};
-
-export default AuthProvider;
+}
