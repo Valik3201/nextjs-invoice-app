ca1de3ce6c53bc292314e9861c0902e82e9bd41d
Valik <valik3201@gmail.com>
2024-05-27 23:24:21 +0200

    feat(ui): Updated invoices page (added loading to display `displayName`)

--- src/app/invoices/page.tsx
+++ src/app/invoices/page.tsx
@@ -5,12 +5,12 @@ import { logout } from "@/src/lib/features/auth/authOperations";
 import AddInvoiceForm from "@/src/components/AddInvoiceForm";
 import { useAppDispatch, useAppSelector } from "@/src/lib/hooks";
 import { fetchInvoices } from "@/src/lib/features/invoices/invoicesOperations";
-import StoreProvider from "@/src/store/StoreProvider";
 
 export default function Page() {
   const router = useRouter();
   const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.auth.user);
+  const loading = useAppSelector((state) => state.auth.loading);
   const invoices = useAppSelector((state) => state.invoices.invoices);
   const error = useAppSelector((state) => state.invoices.error);
 
@@ -39,25 +39,19 @@ export default function Page() {
   };
 
   return (
-    <StoreProvider>
-      <div>
-        {user && (
-          <>
-            <h1 className="text-3xl">
-              Welcome back,{" "}
-              <span className="font-bold">{user.displayName}</span>
-            </h1>
-            <button
-              onClick={handleLogout}
-              className="text-red-medium font-bold"
-            >
-              Logout
-            </button>
+    <div>
+      {user && !loading && (
+        <>
+          <h1 className="text-3xl">
+            Welcome back, <span className="font-bold">{user.displayName}</span>
+          </h1>
+          <button onClick={handleLogout} className="text-red-medium font-bold">
+            Logout
+          </button>
 
-            <AddInvoiceForm />
-          </>
-        )}
-      </div>
-    </StoreProvider>
+          <AddInvoiceForm />
+        </>
+      )}
+    </div>
   );
 }
