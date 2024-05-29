2c10693125b5204fc0cc91deb412e3198673fdae
Valik <valik3201@gmail.com>
2024-05-29 08:32:39 +0200

    feat(ui): Updated no invoices info condition and added image

--- src/app/invoices/page.tsx
+++ src/app/invoices/page.tsx
@@ -9,6 +9,7 @@ import PlusIcon from "@/src/icons/PlusIcon";
 import { formatDate } from "@/src/lib/utils";
 import Status from "@/src/components/Status";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Page() {
   const router = useRouter();
@@ -44,10 +45,10 @@ export default function Page() {
         <>
           {/* <AddInvoiceForm /> */}
 
-          <div className="flex justify-between items-center mb-[68px]">
+          <div className="flex justify-between items-center mb-8 md:mb-[55px] lg:mb-[68px]">
             <div>
               <h1 className="text-heading-l mb-[6px]">Invoices</h1>
-              {invoices ? (
+              {invoices.length !== 0 ? (
                 <p className="text-body-variant text-gray-medium dark:text-gray-light">
                   There are {invoices.length} total invoices
                 </p>
@@ -103,8 +104,16 @@ export default function Page() {
               })}
           </ul>
 
-          {!invoices && (
+          {invoices.length === 0 && (
             <div className="flex flex-col items-center gap-[23px]">
+              <Image
+                src="/no-invoices.svg"
+                alt="No Invoices Info"
+                width={242}
+                height={200}
+                className="mt-[70px] md:mt-[154px] lg:mt-[72px]"
+                priority
+              />
               <h2 className="text-heading-m">There is nothing here</h2>
               <p className="text-body-variant text-gray-medium dark:text-gray-light text-center">
                 Create an invoice by clicking the <br />
