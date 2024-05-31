0e6e966c34687d63d91bdaccce98f2ced46d7242
Valik <valik3201@gmail.com>
2024-06-01 01:05:22 +0200

    chore(assets): Updated logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,10 @@
-import LogoIcon from "../icons/LogoIcon";
-import BgLogo from "./BgLogo";
+import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div className="relative w-fit">
-      <div>
-        <BgLogo />
-      </div>
-
-      <div className="absolute top-0 left-0 z-10 translate-x-[75%] translate-y-[80%]">
-        <LogoIcon />
+    <div className="relative w-[72px] md:w-20 lg:w-full">
+      <div className="relative top-0 left-0 z-10">
+        <Image src="/logo.svg" alt="Logo" width={103} height={103} priority />
       </div>
     </div>
   );
