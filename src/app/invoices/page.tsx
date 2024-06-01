b13293faca2959593aae2fa6e49d8d47ce55405e
Valik <valik3201@gmail.com>
2024-06-01 22:39:22 +0200

    fix(invoices): Fixed check for 0 invoices

--- src/app/invoices/page.tsx
+++ src/app/invoices/page.tsx
@@ -96,7 +96,9 @@ export default function Page() {
                   </p>
                 </>
               ) : (
-                <p>No invoices</p>
+                <p className="text-body-variant text-gray-medium dark:text-gray-light">
+                  No invoices
+                </p>
               )}
             </div>
 
@@ -215,25 +217,24 @@ export default function Page() {
               })}
           </ul>
 
-          {invoices.length === 0 ||
-            (filteredInvoices.length === 0 && (
-              <div className="flex flex-col items-center gap-[23px]">
-                <Image
-                  src="/no-invoices.svg"
-                  alt="No Invoices Info"
-                  width={242}
-                  height={200}
-                  className="mt-[70px] md:mt-[154px] lg:mt-[72px]"
-                  priority
-                />
-                <h2 className="text-heading-m">There is nothing here</h2>
-                <p className="text-body-variant text-gray-medium dark:text-gray-light text-center">
-                  Create an invoice by clicking the <br />
-                  <span className="font-bold">New Invoice</span> button and get
-                  started
-                </p>
-              </div>
-            ))}
+          {(invoices.length === 0 || filteredInvoices.length === 0) && (
+            <div className="flex flex-col items-center gap-[23px]">
+              <Image
+                src="/no-invoices.svg"
+                alt="No Invoices Info"
+                width={242}
+                height={200}
+                className="mt-[70px] md:mt-[154px] lg:mt-[72px]"
+                priority
+              />
+              <h2 className="text-heading-m">There is nothing here</h2>
+              <p className="text-body-variant text-gray-medium dark:text-gray-light text-center">
+                Create an invoice by clicking the <br />
+                <span className="font-bold">New Invoice</span> button and get
+                started
+              </p>
+            </div>
+          )}
         </>
       )}
     </>
