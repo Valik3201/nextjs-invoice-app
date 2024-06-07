48f9a440728ba3b8c3134c578e86bcac0b5fcfd7
Valik <valik3201@gmail.com>
2024-06-07 20:43:52 +0200

    feat(icons): Updated Facebook icon

--- src/icons/FacebookIcon.tsx
+++ src/icons/FacebookIcon.tsx
@@ -1,13 +1,11 @@
 export default function FacebookIcon() {
   return (
     <svg
-      className="w-6 h-6"
+      className="w-4 h-6"
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       fill="currentColor"
-      viewBox="0 0 24 24"
+      viewBox="0 0 16 24"
     >
       <path
         fillRule="evenodd"
