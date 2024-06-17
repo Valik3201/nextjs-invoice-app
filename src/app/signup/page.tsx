47ecd211a78d97e394df02d042ed8e3c0c8c9f50
Valik <valik3201@gmail.com>
2024-06-17 09:20:15 +0200

    refactor(layout): Updated layout and signin/signup pages

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,20 +1,14 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
-import { Formik, Form } from "formik";
-import { signUpValidationSchema } from "@/src/validation/authValidationSchema";
-import { useAppDispatch, useAppSelector, useAppStore } from "@/src/lib/hooks";
 import { resetErrors } from "@/src/lib/features/auth/authSlice";
-import { signUp } from "@/src/lib/features/auth/authOperations";
+import { useAppSelector, useAppStore } from "@/src/lib/hooks";
+import { useAuth } from "@/src/hooks/useAuth";
+import AuthNavigation from "@/src/components/Auth/AuthNavigation";
 import AuthError from "@/src/components/Auth/AuthError";
-import InputField from "@/src/components/FormElements/InputField";
-import Button from "@/src/components/Button/Button";
+import SignUpForm from "@/src/components/Auth/SignUpForm";
 
 export default function SignUp() {
-  const router = useRouter();
-  const dispatch = useAppDispatch();
   const error = useAppSelector((state) => state.auth.errors.registerError);
   const store = useAppStore();
   const initialize = useRef(false);
@@ -33,78 +27,9 @@ export default function SignUp() {
           Create an account
         </h1>
 
-        <Formik
-          initialValues={{
-            name: "",
-            email: "",
-            password: "",
-          }}
-          validationSchema={signUpValidationSchema}
-          validateOnChange={false}
-          onSubmit={async (values) => {
-            const { name, email, password } = values;
+        <SignUpForm />
 
-            try {
-              await dispatch(
-                signUp({ email, password, displayName: name })
-              ).unwrap();
-              router.push("/invoices");
-            } catch (error) {
-              console.log(error);
-            }
-          }}
-        >
-          {(formik) => (
-            <Form className="flex flex-col gap-6">
-              <InputField
-                label="Name"
-                name="name"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.name}
-                error={
-                  formik.touched.name && formik.errors.name
-                    ? "can’t be empty"
-                    : undefined
-                }
-                placeholder="John Doe"
-              />
-
-              <InputField
-                label="Email"
-                name="email"
-                type="email"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.email}
-                error={formik.touched.email && formik.errors.email}
-                placeholder="example@mail.com"
-              />
-
-              <InputField
-                label="Password"
-                name="password"
-                type="password"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.password}
-                error={formik.touched.password && formik.errors.password}
-                placeholder="Password"
-              />
-
-              <Button variant="primary" type="submit" size="full">
-                Sign Up
-              </Button>
-            </Form>
-          )}
-        </Formik>
-
-        <p className="text-body-variant text-gray-medium dark:text-gray-light mt-6">
-          Already have an account?{" "}
-          <Link href={"/signin"} className="text-primary hover:underline">
-            Sign in here
-          </Link>
-        </p>
+        <AuthNavigation />
 
         {error && <AuthError errorCode={error.code} />}
       </div>
