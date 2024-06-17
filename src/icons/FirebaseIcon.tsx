9a6ccdea5ef8f0d80d9ab0648bc7e691069b4b20
Valik <valik3201@gmail.com>
2024-06-17 09:20:22 +0200

    style(icons): Updated Google and Firebase icons

--- src/icons/FirebaseIcon.tsx
+++ src/icons/FirebaseIcon.tsx
@@ -1,11 +1,8 @@
 export default function FirebaseIcon() {
   return (
     <svg
-      version="1.1"
-      id="Layer_1"
+      className="w-6 h-6"
       xmlns="http://www.w3.org/2000/svg"
-      x="0px"
-      y="0px"
       viewBox="0 0 220 220"
     >
       <g>
