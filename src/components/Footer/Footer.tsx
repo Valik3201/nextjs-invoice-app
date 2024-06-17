f4de301183569ad4ed2e5a5ce681eba6a56751e6
Valik <valik3201@gmail.com>
2024-06-17 09:19:58 +0200

    fix(ui): Updated Footer to always be at the bottom

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="w-[327px] md:w-[672px] lg:w-[730px] mx-auto flex flex-col gap-4 pb-8">
+    <footer className="mt-auto w-[327px] md:w-[672px] lg:w-[730px] mx-auto flex flex-col gap-4 pb-8 mb-20 md:mb-0">
       <div className="h-px w-full bg-gray-light dark:bg-dark-medium"></div>
 
       <div className="flex gap-2 items-center justify-center pt-4">
