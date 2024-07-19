ac1936617828159efe46c34439e5a6bc34d88282
Valik <valik3201@gmail.com>
2024-07-19 22:30:43 +0200

    chore: Remove Tailwind CSS

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -14,8 +14,15 @@ export const dynamicParams = true;
 
 export default async function Image({ params }: { params: string[] }) {
   const leagueSpartan = await fetch(
-    "https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap"
-  ).then((res) => res.arrayBuffer());
+    new URL(
+      "https://fonts.googleapis.com/css2?family=League+Spartan:wght@400&display=swap"
+    ).href
+  ).then((res) => res.text());
+
+  const fontSrc = leagueSpartan.match(/@font-face {([^}]+)}/g)?.[0];
+  const fontUrl = fontSrc?.match(/url\(([^)]+)\)/)?.[1];
+
+  const fontData = await fetch(fontUrl!).then((res) => res.arrayBuffer());
 
   let title = "";
   let description = "";
@@ -48,38 +55,51 @@ export default async function Image({ params }: { params: string[] }) {
   return new ImageResponse(
     (
       <div
-        tw="h-full w-full flex flex-col align-start justify-center py-10 px-20"
         style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "start",
+          padding: "10px 20px",
           background: "#F8F8FB",
-          backgroundSize: "100% 100%",
           fontFamily: "League Spartan",
+          backgroundSize: "100% 100%",
         }}
       >
-        <div tw="flex">
-          <div tw="flex items-center">
-            <div tw="flex">
+        <div style={{ display: "flex" }}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <div style={{ display: "flex" }}>
               <img
                 width="100"
                 height="100"
                 src={`/logo-sm.png`}
-                tw="mr-10 mt-6"
+                style={{ marginRight: "10px", marginTop: "6px" }}
               />
-
-              <div tw="flex flex-col">
-                <h1 tw="text-8xl font-extrabold tracking-tight leading-none mb-6 whitespace-pre-wrap">
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <h1
+                  style={{
+                    fontSize: "64px",
+                    fontWeight: "800",
+                    lineHeight: "none",
+                    marginBottom: "6px",
+                  }}
+                >
                   {title}
                 </h1>
-                <p tw="text-2xl max-w-96">{description}</p>
+                <p style={{ fontSize: "24px", maxWidth: "24rem" }}>
+                  {description}
+                </p>
               </div>
             </div>
           </div>
-
-          <div tw="flex">
+          <div style={{ display: "flex" }}>
             <img
               width="628"
               height="760"
               src={`/mockups/mockup-1.png`}
-              tw="mr-10"
+              style={{ marginRight: "10px" }}
             />
           </div>
         </div>
@@ -89,11 +109,13 @@ export default async function Image({ params }: { params: string[] }) {
       fonts: [
         {
           name: "League Spartan",
-          data: leagueSpartan,
+          data: fontData,
           style: "normal",
           weight: 400,
         },
       ],
+      width: size.width,
+      height: size.height,
     }
   );
 }
